a2537da0309456c46d19b07cf9c419699bbef7f7
Song Liu <songliubraving@fb.com>
2019-05-16 10:56:44 -0700

    dump pgtable for given pid

--- arch/x86/mm/debug_pagetables.c
+++ arch/x86/mm/debug_pagetables.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include <linux/debugfs.h>
 #include <linux/efi.h>
+#include <linux/sched/task.h>
 #include <linux/module.h>
 #include <linux/seq_file.h>
 #include <asm/pgtable.h>
@@ -54,7 +55,36 @@ static int ptdump_efi_show(struct seq_file *m, void *v)
 DEFINE_SHOW_ATTRIBUTE(ptdump_efi);
 #endif
 
-static struct dentry *dir, *pe_knl, *pe_curknl;
+pid_t sysctl_dump_pt_pid __read_mostly = -1;
+static struct dentry *pe_pid;
+
+static int ptdump_pid_show(struct seq_file *m, void *v)
+{
+	struct task_struct *tsk;
+	struct pid *pid;
+
+	pid = find_get_pid(sysctl_dump_pt_pid);
+	if (!pid)
+		return 0;
+	tsk = get_pid_task(pid, PIDTYPE_PID);
+	if (!tsk) {
+		put_pid(pid);
+		return 0;
+	}
+
+	if (tsk->mm->pgd) {
+		down_read(&tsk->mm->mmap_sem);
+		ptdump_walk_pgd_level_debugfs(m, tsk->mm->pgd, true);
+		up_read(&tsk->mm->mmap_sem);
+	}
+	put_task_struct(tsk);
+	put_pid(pid);
+	return 0;
+}
+
+DEFINE_SHOW_ATTRIBUTE(ptdump_pid);
+
+static struct dentry *dir, *pe_knl, *pe_curknl, *pe_pid;
 
 static int __init pt_dump_debug_init(void)
 {
@@ -84,6 +114,9 @@ static int __init pt_dump_debug_init(void)
 	if (!pe_efi)
 		goto err;
 #endif
+	pe_pid = debugfs_create_file("dump_pid", 0400, dir, NULL, &ptdump_pid_fops);
+	if (!pe_pid)
+		goto err;
 
 	return 0;
 err:
